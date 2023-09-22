c8c0add625280e8ba7661c8680c5cbe15c9ea270
Minu <122645470+Minusuke@users.noreply.github.com>
2023-09-22 20:18:55 +0000

    corregido

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,8 +5,6 @@ import ResetButton from "/src/js/component/ResetButton.jsx";
 import "../../styles/index.css";
 
 const Home = () => {
-  // Almacenar el estado actual del tablero del juego, el turno y el ganador.
-
   const [gameBoard, setGameBoard] = useState([
     "",
     "",
@@ -18,59 +16,56 @@ const Home = () => {
     "",
     "",
   ]);
-  const [turn, setTurn] = useState(""); // El jugador cuyo turno es actualmente ("X" o "O")
-  const [winner, setWinner] = useState(""); // El jugador ganador ("X", "O")
+  const [turn, setTurn] = useState("");
+  const [winner, setWinner] = useState("");
+  const [playerName, setPlayerName] = useState(""); // Agregamos el estado playerName
 
-  // Función para establecer el Arma para comenzar el juego
   const Weapon = (startWeapon) => {
     setTurn(startWeapon);
   };
 
-  // Función para manejar el click en una celda del tablero
   const updateBoard = (location) => {
-    // Verificar si la celda está vacía y no hay ganador
     if (gameBoard[location] === "" && !winner) {
-      const newBoard = [...gameBoard]; //Crea una copia nueva del array gameBoard
-      newBoard[location] = turn; // Colocar el símbolo del jugador actual en la celda
-      setGameBoard(newBoard); // Actualizar el tablero
+      const newBoard = [...gameBoard];
+      newBoard[location] = turn;
+      setGameBoard(newBoard);
 
-      const newWinner = checkWinner(newBoard); // Verificar si hay un ganador
+      const newWinner = checkWinner(newBoard);
       if (newWinner) {
-        setWinner(newWinner); // Establecer al ganador si lo hay
+        setWinner(newWinner);
       } else {
-        setTurn(turn === "X" ? "O" : "X"); // Alternar el turno entre "X" y "O"
+        setTurn(turn === "X" ? "O" : "X");
       }
     }
   };
-  const isBoardFull = gameBoard.every((cell) => cell !== ""); // Verificar si todas las celdas están llenas
 
-  // Función para verificar si hay un ganador basado en patrones de victoria predefinidas
+  const isBoardFull = gameBoard.every((cell) => cell !== "");
+
   const checkWinner = (board) => {
     const winningPatterns = [
       [0, 1, 2],
       [3, 4, 5],
-      [6, 7, 8], // Filas
+      [6, 7, 8],
       [0, 3, 6],
       [1, 4, 7],
-      [2, 5, 8], // Columnas
+      [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6], // Diagonales
+      [2, 4, 6],
     ];
 
     for (const pattern of winningPatterns) {
       const [a, b, c] = pattern;
       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        return board[a]; // Retorna "X" o "O", el jugador ganador
+        return board[a];
       }
     }
-    return null; // Si no hay ganador
+    return null;
   };
 
-  // Función para reiniciar el juego
   const resetBoard = () => {
-    setGameBoard(["", "", "", "", "", "", "", "", ""]); // Reiniciar el tablero
-    setTurn(""); // Reiniciar el turno a vacío
-    setWinner(""); // Reiniciar el ganador
+    setGameBoard(["", "", "", "", "", "", "", "", ""]);
+    setTurn("");
+    setWinner("");
   };
 
   return (
@@ -83,6 +78,13 @@ const Home = () => {
           <button onClick={() => Weapon("X")}>Start as X</button>
           <button onClick={() => Weapon("O")}>Start as O</button>
         </div>
+        {/* Espacio para que el jugador añada su nombre */}
+        <input
+          type="text"
+          placeholder="Nombre del jugador"
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+        />
       </div>
       {gameBoard.map((value, i) => (
         <Tile
